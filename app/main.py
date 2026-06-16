ea1d9563294560b45ff6db9205e16438d9690146
vaardhanraju <vaardhanraju@gmail.com>
2026-06-16 18:23:46 +0000

    refactor: add startup event

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.db.database import create_db_and_tables
 from app.routes.auth import router as auth_router
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 app.include_router(auth_router)
